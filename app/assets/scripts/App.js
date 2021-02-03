d6e899689f10f50e1d791519d6aafaf1fbb0ca6d
stu <stthall@icloud.com>
2021-02-03 10:23:38 -0800

    +modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,7 +4,22 @@ import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 
-let stickyHeader = new StickyHeader()
-let mobileMenu = new MobileMenu();
+new MobileMenu();
+new StickyHeader()
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75)
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 65)
+let modal;
+
+document.querySelectorAll('.open-modal').forEach( el => {
+  el.addEventListener('click', e => {
+    e.preventDefault()
+    if(typeof modal == 'undefined'){
+      import(/* webpackChunkName: "modal" */'./modules/Modal').then( modalFile => {
+        modal = new modalFile.default()
+        setTimeout( () => modal.openTheModal(), 20)
+      }).catch( e => console.log('There was a problem: '+e.message))
+    }else{
+      modal.openTheModal()
+    }
+  })
+})

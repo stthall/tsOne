acf8ce271a0732a0c5a9d8b783176a176cb88152
stu <stthall@icloud.com>
2021-02-11 08:27:01 -0800

    bug fixes

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const postCSSPlugins = [
   require('postcss-mixins'),
   require('postcss-simple-vars'),
   require('postcss-nested'),
-  require('postcss-hexrgba'),
+  // require('postcss-hexrgba'),
   require('autoprefixer')
 ]
 
